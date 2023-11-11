ed2580356d9d8eb15d144ab5a3af6e7b0c731ba5
Kozenko Valeriy <Osmiylantan@mail.ru>
2023-11-11 20:31:02 +0300

    Add service layer for phone address redis db with get and set functions.

---
+++ app/core/exceptions.py
@@ -0,0 +1,2 @@
+class NoEntityError(Exception):
+    pass
---
+++ app/core/service.py
@@ -0,0 +1,42 @@
+from app.core.repositories import PhoneAddressRepository
+from app.core import exceptions as exc
+
+
+async def create_update_data(
+        phone: str,
+        address: str,
+        phone_address_repository: PhoneAddressRepository,
+) -> None:
+    """
+    Create or update data.
+
+    :param phone: phone
+    :type phone: str
+    :param address: address
+    :type address: str
+    :param phone_address_repository: phone address repository
+    :type phone_address_repository: PhoneAddressRepository
+    :return: None
+    :rtype: Coroutine[None, None, None]
+    """
+    await phone_address_repository.add(phone, address)
+
+
+async def get_data(
+        phone: str,
+        phone_address_repository: PhoneAddressRepository,
+) -> bytes:
+    """
+    Get data by phone.
+
+    :param phone: phone
+    :type phone: str
+    :param phone_address_repository: phone address repository
+    :type phone_address_repository: PhoneAddressRepository
+    :return: corresponding address
+    :rtype: Coroutine[None, None, bytes]
+    """
+    address = await phone_address_repository.get(phone)
+    if address:
+        return address
+    raise exc.NoEntityError(f"There is no entity with phone {phone}")
